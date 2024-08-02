43d32cc87c3f8f539fd5d7e498a232a580759b15
DDchengPi <86303506+DDchengPi@users.noreply.github.com>
2024-08-02 17:27:13 +0800

    Add files via upload

---
+++ test_simple.py
@@ -0,0 +1,13 @@
+# from selenium import webdriver
+#
+# wd = webdriver.Chrome()
+#
+# # 2024.7.8 晚
+# # 待做，电脑重启后运行检查环境变量中G:\peng_learning\chromedriver-win64\chromedriver.exe是否删除
+#
+# wd.get('https://www.baidu.com')
+#
+# input('')
+def test_a():
+    print("hello world")
+    assert 1+1 == 2 
